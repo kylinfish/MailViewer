b1883021b2d79aeadf6506a5125d95a0426bfddf
win <win@pixnet.tw>
2016-05-16 13:06:36 +0800

    style

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 (function($) {
-  $(document).on('dblclick', 'table', fillcolor);
+    $(document).on('dblclick', 'table', fillcolor);
     $(document).on('keydown', function(e) {
         switch(e.keyCode) {
             case 37: // left
@@ -17,17 +17,17 @@
 })(jQuery);
 
 function fillcolor() {
-  var lines = $('.a3s')[0].innerHTML.split('\n');
-  if (lines[0].indexOf('Commit') <= -1) return;
-  var newBody = '';
-  for (var i = 0; i <= lines.length; i++) {
-    if (lines[i] === undefined) break;
-    if (lines[i].charAt(0) == '+') {
-      lines[i] = '<div style=\'background-color: #d0ffd0; width: 100%;\'>' + lines[i] + '</div>';
-    } else if (lines[i].charAt(0) == '-') {
-      lines[i] = '<div style=\'background-color: #ffd0d0; width: 100%;\'>' + lines[i] + '</div>';
+    var lines = $('.a3s')[0].innerHTML.split('\n');
+    if (lines[0].indexOf('Commit') <= -1) return;
+    var newBody = '';
+    for (var i = 0; i <= lines.length; i++) {
+        if (lines[i] === undefined) break;
+        if (lines[i].charAt(0) == '+') {
+            lines[i] = '<div style=\'background-color: #d0ffd0; width: 100%;\'>' + lines[i] + '</div>';
+        } else if (lines[i].charAt(0) == '-') {
+            lines[i] = '<div style=\'background-color: #ffd0d0; width: 100%;\'>' + lines[i] + '</div>';
+        }
+        newBody += lines[i];
     }
-    newBody += lines[i];
-  }
-  $('.a3s')[0].innerHTML = newBody;
+    $('.a3s')[0].innerHTML = newBody;
 }
